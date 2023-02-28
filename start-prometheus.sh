a38dc6a20f0b1ada8491747774ebd1c676e7c9f1
John Valentino <jvalentino2@gmail.com>
2023-02-28 13:51:08 -0600

    got prometheus able to find spring boot

--- start-prometheus.sh
+++ start-prometheus.sh
@@ -7,14 +7,14 @@ pkill -f 9093
 pkill -f 9091
 
 sleep 10
-helm install -f config/helm/prometheus/values.yaml prometheus prometheus-community/prometheus
+helm install -f config/helm/prometheus/values.yaml prometheus prometheus-community/kube-prometheus-stack
 sleep 60
 
-POD_NAME=$(kubectl get pods --namespace default -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=prometheus" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace default port-forward $POD_NAME 9090 > build/prometheus-server.log 2>&1 &
 
-POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=alertmanager" -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pods --namespace default -l "alertmanager=prometheus-kube-prometheus-alertmanager" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace default port-forward $POD_NAME 9093 > build/alert-manager.log 2>&1 &
 
-POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=prometheus-pushgateway" -o jsonpath="{.items[0].metadata.name}")
-kubectl --namespace default port-forward $POD_NAME 9091 > build/pushgateway.log 2>&1 &
+# POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=prometheus-pushgateway" -o jsonpath="{.items[0].metadata.name}")
+#kubectl --namespace default port-forward $POD_NAME 9091 > build/pushgateway.log 2>&1 &
