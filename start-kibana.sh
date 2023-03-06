7665407775bb08a03237536527d3d1b81d2a9d34
John Valentino <jvalentino2@gmail.com>
2023-03-06 13:35:58 -0600

    got kibana working

--- start-kibana.sh
+++ start-kibana.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 kubectl delete deploy kibana-deployment || true
+kubectl delete service kibana || true
+kubectl delete configmaps kibana-config || true
 kubectl create -f ./config/helm/kibana/deployment.yaml
+# kubectl apply -f ./config/helm/kibana/deployment.yaml
 
 kubectl wait pods -l app=kibana --for condition=Ready
 
