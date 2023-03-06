108a44c9c3f31a992a883a1821b7ad6904a29690
John Valentino <jvalentino2@gmail.com>
2023-03-06 16:18:21 -0600

    cluster health shard as yellow is good

--- start-backend.sh
+++ start-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-helm delete backend
-sleep 10
-helm install backend config/helm/backend/ --values config/helm/backend/values.yaml
+helm delete backend --wait
+helm install backend --wait config/helm/backend/ --values config/helm/backend/values.yaml
+kubectl port-forward --namespace default svc/backend 8080:8080 > build/backend.log 2>&1 &
--- start-db.sh
+++ start-db.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm delete pg-primary || true
-sleep 10
+helm delete pg-primary --wait || true
 helm install pg-primary \
+	--wait \
 	--set auth.postgresPassword=postgres \
 	--set auth.username=postgres \
 	--set auth.database=examplesys \
 	bitnami/postgresql
+
+kubectl port-forward --namespace default svc/pg-primary-postgresql 5432:5432 > build/pg-primary-postgresql.log 2>&1 &
